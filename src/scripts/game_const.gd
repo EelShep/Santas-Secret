06582d16f568eeb6ac2a658bb455775984b3de6b
Eelshep <eelshep672@gmail.com>
2024-11-05 14:21:32 -0600

    Squashed commit of the following:
    commit 005ac99741194331bf8bf239b75fad0e85bfc355
    Author: Eelshep <eelshep672@gmail.com>
    Date:   Tue Nov 5 09:22:50 2024 -0600
    
        Project settings updated
    
    commit 968db97ab6d7888db1db15aebf69ed579c19df68
    Author: Eelshep <eelshep672@gmail.com>
    Date:   Tue Nov 5 09:22:22 2024 -0600
    
        Revert "-"
    
        This reverts commit c127d70614f1ff4ef0d6302d5d1074c0bee7af9c.
    
    commit c127d70614f1ff4ef0d6302d5d1074c0bee7af9c
    Author: Elias Poniridis <68995203+Creperi@users.noreply.github.com>
    Date:   Tue Nov 5 10:57:16 2024 +0200
    
        -
    
    commit c38ce842bf65b95e3e21d1d9aa9eada5514277ed
    Author: Eelshep <eelshep672@gmail.com>
    Date:   Mon Nov 4 09:41:41 2024 -0600
    
        Squashed commit of the following:
    
        commit 2315ccbcac276ecfe825aa7b43c43de5f8f5239c
        Author: Eelshep <eelshep672@gmail.com>
        Date:   Mon Nov 4 09:41:33 2024 -0600
    
            Transition to Main Menu
    
            - Transition from Game to Main Menu added
    
        commit 9b1a3b439430d419f47092c194b9742eafdd401d
        Author: Eelshep <eelshep672@gmail.com>
        Date:   Mon Nov 4 09:38:29 2024 -0600
    
            Basic Scene Transitions added
    
            - New Global SceneTransition added
    
    commit 03317eb87bfc3a96c4b2972d420420dea9150330
    Author: Eelshep <eelshep672@gmail.com>
    Date:   Mon Nov 4 09:26:08 2024 -0600
    
        Squashed commit of the following:
    
        commit ee6ee11f574147d34a31dbad9dc431fffe6a7857
        Author: Eelshep <eelshep672@gmail.com>
        Date:   Mon Nov 4 09:25:54 2024 -0600
    
            Static Refs & GameConst inputs
    
            - Level & TestPlayer hold static refs
            - New Project inputs & references to them in GameConst
    
        commit fbce20f344c512a4b01f9275465de26289de4754
        Author: Eelshep <eelshep672@gmail.com>
        Date:   Sun Nov 3 14:36:27 2024 -0600
    
            Test Player & Level added
    
    commit 3550c3f5e2546106060b470d18c393d062bd17b8
    Author: Eelshep <eelshep672@gmail.com>
    Date:   Sun Nov 3 09:47:22 2024 -0600
    
        testing webhook
    
    commit 73368df0974de61123791e921aa6cc29aa1a284d
    Author: Eelshep <eelshep672@gmail.com>
    Date:   Sun Nov 3 09:42:45 2024 -0600
    
        Pause Menu Added
    
        - Pause menu added to Game scene
        - GameData Global script added
        - Play time is displayed
    
    commit e8d86d25214c12f52165e89feb3723b3cca02b3e
    Author: Eelshep <eelshep672@gmail.com>
    Date:   Sat Nov 2 14:40:42 2024 -0500
    
        Fixed Placeholder Music

--- src/scripts/game_const.gd
+++ src/scripts/game_const.gd
@@ -3,9 +3,10 @@ class_name GameConst extends RefCounted
 #region Inputs
 const INPUT_SELECT: String = "select"
 const INPUT_START: String = "start"
-const INPUT_FORWARD: String = "forward"
-const INPUT_BACKWARD: String = "backward"
-const INPUT_LEFT: String = "left"
-const INPUT_RIGHT: String = "right"
+const INPUT_MOVE_UP: String = "move_up"
+const INPUT_MOVE_DOWN: String = "move_down"
+const INPUT_MOVE_LEFT: String = "move_left"
+const INPUT_MOVE_RIGHT: String = "move_right"
+const INPUT_JUMP: String = "jump"
 const INPUT_USE: String = "use"
 #endregion
