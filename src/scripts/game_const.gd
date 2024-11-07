552b03dee7b48fdbff520a27af531d41cfdfb5f3
Eelshep <eelshep672@gmail.com>
2024-11-07 09:17:29 -0600

    Squashed commit of the following:
    commit 9dc3ffa09b0450b52e13df8f8f06d58499615e44
    Author: Rudr <148496989+Yu-xo@users.noreply.github.com>
    Date:   Thu Nov 7 07:23:33 2024 +0530
    
        ..
    
    commit d0e4560a47e4b96327d9f346ce27deafe5cce95b
    Author: Rudr <148496989+Yu-xo@users.noreply.github.com>
    Date:   Thu Nov 7 07:13:10 2024 +0530
    
        Throw
    
        I have added the throw mech in the player script
    
    commit 1fe1aa982e2d64efe060dc4ec9df04cfff08a92f
    Author: Eelshep <eelshep672@gmail.com>
    Date:   Wed Nov 6 18:07:56 2024 -0600
    
        Map bug fix
    
        - Fixed broken dependancies
    
    commit 85f887f8328ec752d4992683995e977f0327bf63
    Author: Eelshep <eelshep672@gmail.com>
    Date:   Wed Nov 6 16:45:04 2024 -0600
    
        Day Night  & Lighting System
    
        - Default, Player & Tilemap Light layers added
        - DayNight CanvasModulate added
        - Player Light added
        - Press R to reload tree
    
    commit c0c945fec2bc24b9c7accbbda283ac0fc4bb3e72
    Author: Eelshep <eelshep672@gmail.com>
    Date:   Wed Nov 6 10:39:31 2024 -0600
    
        Dialogue Manager & Interaction System
    
        - DialogueManager addon added
        - Interactable class added
        - Player Interaction finder added
        - NPC and Dialogue Interactable added
    
    commit c9afbf34fc15abb952c9c09d194b624a68e6fb37
    Author: Eelshep <eelshep672@gmail.com>
    Date:   Tue Nov 5 14:36:20 2024 -0600
    
        MetSys Adjustments
    
        - Export includes MetSys MapData
        - Game music activated
    
    commit c23eae327dadba2c64045e0dd904ddaceb931732
    Author: Eelshep <eelshep672@gmail.com>
    Date:   Tue Nov 5 13:09:27 2024 -0600
    
        MetSys Added
    
    commit 005ac99741194331bf8bf239b75fad0e85bfc355
    Author: Eelshep <eelshep672@gmail.com>
    Date:   Tue Nov 5 09:22:50 2024 -0600
    
        Project settings updated
    
    commit 968db97ab6d7888db1db15aebf69ed579c19df68
    Author: Eelshep <eelshep672@gmail.com>
    Date:   Tue Nov 5 09:22:22 2024 -0600
    
        Revert "-"
    
        This reverts commit c127d70614f1ff4ef0d6302d5d1074c0bee7af9c.
    
    commit c127d70614f1ff4ef0d6302d5d1074c0bee7af9c
    Author: Elias Poniridis <68995203+Creperi@users.noreply.github.com>
    Date:   Tue Nov 5 10:57:16 2024 +0200
    
        -
    
    commit c38ce842bf65b95e3e21d1d9aa9eada5514277ed
    Author: Eelshep <eelshep672@gmail.com>
    Date:   Mon Nov 4 09:41:41 2024 -0600
    
        Squashed commit of the following:
    
        commit 2315ccbcac276ecfe825aa7b43c43de5f8f5239c
        Author: Eelshep <eelshep672@gmail.com>
        Date:   Mon Nov 4 09:41:33 2024 -0600
    
            Transition to Main Menu
    
            - Transition from Game to Main Menu added
    
        commit 9b1a3b439430d419f47092c194b9742eafdd401d
        Author: Eelshep <eelshep672@gmail.com>
        Date:   Mon Nov 4 09:38:29 2024 -0600
    
            Basic Scene Transitions added
    
            - New Global SceneTransition added
    
    commit 03317eb87bfc3a96c4b2972d420420dea9150330
    Author: Eelshep <eelshep672@gmail.com>
    Date:   Mon Nov 4 09:26:08 2024 -0600
    
        Squashed commit of the following:
    
        commit ee6ee11f574147d34a31dbad9dc431fffe6a7857
        Author: Eelshep <eelshep672@gmail.com>
        Date:   Mon Nov 4 09:25:54 2024 -0600
    
            Static Refs & GameConst inputs
    
            - Level & TestPlayer hold static refs
            - New Project inputs & references to them in GameConst
    
        commit fbce20f344c512a4b01f9275465de26289de4754
        Author: Eelshep <eelshep672@gmail.com>
        Date:   Sun Nov 3 14:36:27 2024 -0600
    
            Test Player & Level added
    
    commit 3550c3f5e2546106060b470d18c393d062bd17b8
    Author: Eelshep <eelshep672@gmail.com>
    Date:   Sun Nov 3 09:47:22 2024 -0600
    
        testing webhook
    
    commit 73368df0974de61123791e921aa6cc29aa1a284d
    Author: Eelshep <eelshep672@gmail.com>
    Date:   Sun Nov 3 09:42:45 2024 -0600
    
        Pause Menu Added
    
        - Pause menu added to Game scene
        - GameData Global script added
        - Play time is displayed
    
    commit e8d86d25214c12f52165e89feb3723b3cca02b3e
    Author: Eelshep <eelshep672@gmail.com>
    Date:   Sat Nov 2 14:40:42 2024 -0500
    
        Fixed Placeholder Music

--- src/scripts/game_const.gd
+++ src/scripts/game_const.gd
@@ -8,5 +8,11 @@ const INPUT_MOVE_DOWN: String = "move_down"
 const INPUT_MOVE_LEFT: String = "move_left"
 const INPUT_MOVE_RIGHT: String = "move_right"
 const INPUT_JUMP: String = "jump"
-const INPUT_USE: String = "use"
+const INPUT_INTERACT: String = "interact"
+#endregion
+#region CollisionLayers
+const COLLISION_WORLD: int = 0
+const COLLISION_PLAYER: int = 1
+const COLLISION_ENEMY: int = 2
+const COLLISION_INTERACTABLE: int = 3
 #endregion
