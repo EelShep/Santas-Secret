82685d1f41f65c56d723e7a3e931b3c6c759d334
Eelshep <eelshep672@gmail.com>
2024-11-08 10:04:36 -0600

    Squashed commit of the following:
    commit e579d800f2c5a4b9361d6873918d2c1be200ac35
    Author: Eelshep <eelshep672@gmail.com>
    Date:   Fri Nov 8 10:02:09 2024 -0600
    
        GameUI setup
    
        - play_time & day_night status labels displayed
    
    commit 2de173568b6f33f5e6172ab31e44e828fa8bd569
    Author: Eelshep <eelshep672@gmail.com>
    Date:   Fri Nov 8 09:41:04 2024 -0600
    
        Time & Lighting adjustments
    
        - Player & NPC now receive light
        - Player light turns on & off at certain hours
        - Fade transition when entering a room

--- scenes/main/met_game.gd
+++ scenes/main/met_game.gd
@@ -7,8 +7,9 @@ const SAVE_PATH = "user://example_save_data.sav"
 @export var starting_map: String
 @export_category("Setup")
 @export var level_manager: Node2D
+@export var game_ui: GameUI
 @export var day_night: DayNight
-@export var play_time_label: Label
+#@export var play_time_label: Label
 # The coordinates of generated rooms. MetSys does not keep this list, so it needs to be done manually.
 var generated_rooms: Array[Vector3i]
 var custom_run: bool # For Custom Runner integration.
@@ -18,7 +19,7 @@ func _init() -> void:
 	if instance == null: print("Previous instance: " + str(self))
 	instance = self
 
-signal new_second
+signal new_second(play_time: float)
 var total_play_time: float
 var last_second: int = -1
 var play_time: float:
@@ -27,25 +28,32 @@ var play_time: float:
 		var current_second: int = int(play_time)
 		if current_second == last_second: return
 		last_second = current_second
-		new_second.emit()
+		new_second.emit(play_time)
 func _process(delta: float) -> void:
 	total_play_time += delta
 	play_time += delta
 	
 #region Signal Functions
-func _on_new_second() -> void:
-	play_time_label.text = GameData.convert_time(play_time)
+
 	
 func _on_events_dialogue_toggle(value: bool) -> void:
 	Events.can_pause.emit(!value)
 	get_tree().paused = value
 #endregion
-	
+
+func _on_day_night_time_tick(day:int, hour:int, minute:int) -> void:
+	if hour == 8 || hour == 19:
+		var player: TestPlayer = TestPlayer.instance
+		player.on_day_night(hour)
+		game_ui.on_day_night(hour)
+	#if hour == 
 
 func _ready() -> void:
+	#NOTE connect signals
 	Events.game_ready.emit()
 	Events.dialogue_toggle.connect(_on_events_dialogue_toggle)
-	new_second.connect(_on_new_second)
+	game_ui.setup(self)
+	day_night.time_tick.connect(_on_day_night_time_tick)
 	
 	MetSys.reset_state() # Make sure MetSys is in initial state.
 	set_player($TestPlayer) # Assign player for MetSysGame.
@@ -91,6 +99,7 @@ func reset_map_starting_coords():
 
 func init_room():
 	MetSys.get_current_room_instance().adjust_camera_limits(%Camera2D)
+	SceneTransition.fade_in()
 	player.on_enter()
 
 
