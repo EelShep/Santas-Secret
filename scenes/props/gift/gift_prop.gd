5e0d8a50b49e15946b7ad9fed523ea9156d0705e
Josep Valls <josep@valls.name>
2024-11-14 09:01:31 -0800

    gift piles

---
+++ scenes/props/gift/gift_prop.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+@export var colors = ["f7f3c3", "ffd445", "ff2424", "8815b2", "4da6ff","116666","8fde5d"]
+@export var stack_height = 14
+@export var height = 14
+@export var width = 16
+@export var wraps = 5
+func _ready() -> void:
+	colors.shuffle()
+	var wrap = randi()%wraps
+	$wrap.region_rect = Rect2(width*wrap,0,width,height)
+	$base.modulate = Color(colors[0])
+	$wrap.modulate = Color(colors[1])
+	$bow1.modulate = Color(colors[2])
+	$bow2.modulate = Color(colors[2])
