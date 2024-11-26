c830640b500199d4f6a7fd83bbb4e78cdd532fcb
Eelshep <eelshep672@gmail.com>
2024-11-26 11:46:47 -0600

    Box Gravity

--- scenes/pushable/pushable_box.gd
+++ scenes/pushable/pushable_box.gd
@@ -4,7 +4,14 @@ const PUSH_FORCE: float = 500
 const MAX_PUSH_SPEED: float = 400
 const FRICTION: float = 5000
 
+var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
+
 func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity.x = 0
+		position.x = round((position.x - 32) / 64) * 64 + 32
+		
+	velocity.y += gravity * delta
 	move_and_slide()
 	velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
 
